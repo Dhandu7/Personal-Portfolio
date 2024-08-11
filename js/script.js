af93e8302237a8d72274fd5b7e59cd37eff152f6
Aaryan Dhand <aaryandhand@gmail.com>
2024-08-10 23:15:47 -0600

    edit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,2 @@
-document.getElementById('toggleBtn').addEventListener('click', function() {
-    var navbar = document.getElementById('leftNavbar');
-    var content = document.querySelector('.content');
-    
-    if (navbar.style.left === '0px') {
-        navbar.style.left = '-250px';
-        content.style.marginLeft = '0';
-    } else {
-        navbar.style.left = '0';
-        content.style.marginLeft = '250px';
-    }
-});
+
 
